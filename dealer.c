768f5a70048a1869bc06e9403353aa83a9b2b285
Joseth Daniel Holman <jdh160630@cslinux1.utdallas.edu>
2018-12-10 22:05:56 -0600

    dealer (parameters, forking and exec(not perfect), percentage of successes, some error checking)

--- dealer.c
+++ dealer.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h> 
+#include <sys/wait.h>
 
 int 
 main(int argc, char *argv[]) {
@@ -14,12 +16,14 @@ main(int argc, char *argv[]) {
   
   int index;
   
+  pid_t pid;
+  
   pcnt = 0;
   vflag = 0;
   numt = 0;
   pflag = 0;
   nflag = 0;
-  
+
   while ((opt = getopt(argc, argv, "p:vn:")) != -1)
     switch (opt) {
     case 'p':
@@ -33,15 +37,73 @@ main(int argc, char *argv[]) {
       numt = atoi(optarg);
       nflag = 1;
       break;
+    case '?':
+      fprintf(stderr, "%s: Unknown input\n", argv[0]);
   }
   
+  if (!pflag || !nflag)
+  {
+    fprintf(stderr, "%s: Requires -p AND -n.\n", argv[0]);
+    exit(1);
+  }
+  else if ((pcnt > 100) || (pcnt == 0))
+  {
+    fprintf(stderr, "%s: Invalid percentage value.\n", argv[0]);
+    exit(1);
+  }
+  else if (numt == 0)
+  {
+    fprintf(stderr, "%s: Invalid number_of_trials value.\n", argv[0]);
+    exit(1);
+  }
   
-  
-  printf ("pcnt = %d, vflag = %d, numt = %d\n",
-          pcnt, vflag, numt);
+  //printf ("pcnt = %d, vflag = %d, numt = %d\n", pcnt, vflag, numt);
 
-  for (index = optind; index < argc; index++)
-    printf ("Non-option argument %s\n", argv[index]);
+  //for (index = optind; index < argc; index++)
+    //printf ("Non-option argument %s\n", argv[index]);
+    
+  int result, wins, losses;
+  wins = 0;
+  losses = 0;
+  int childs;
+  childs = 0;
+  
+  for(index = 0; index < numt;index++) {
+    pid = vfork();
+  
+    if(pid == 0) 
+    {
+      result = execv("hand", argv);
+      exit(0);
+    }
+    else if (pid > 0)
+    {
+      if (vflag)
+        printf("PID %i returned \n", pid); 
+
+      pid = wait(&result);   
+      if (WEXITSTATUS(result) == 0)
+        wins++;
+      else
+        losses++;
+    }
+    else 
+    {
+      fprintf(stderr, "%s: Fork failed.\n", argv[0]);
+      exit(1)
+    }
+  }
+  
+  double winrate;
+  winrate = wins/(float)numt;
+  double lossrate;
+  lossrate = losses/(float)numt;
+  
+  printf("\n\nCreated %i processes.\n", numt);
+  printf("Success - %i%%\n", (int)(winrate*100));
+  printf("Failure - %i%%\n", (int)(lossrate*100));
+  
+  
   
   return 0;
 }
--- hand.c
+++ hand.c
@@ -9,18 +9,33 @@ main(int argc, char *argv[]) {
   extern int optind, optopt;
   int opt;
   
-  int pcnt, pflag;
+  int pcnt, pflag, vflag;
   
   pcnt = 0;
   pflag = 0;
+  vflag = 0;
   
-  while ((opt = getopt(argc, argv, "p:")) != -1)
+  while ((opt = getopt(argc, argv, "p:vn")) != -1)
     switch(opt) {
     case 'p':
       pcnt = atoi(optarg);
       pflag = 1;
       break;
+    case 'v':
+      vflag = 1;
+      break;
     }
+    
+  if (!pflag)
+  {
+    fprintf(stderr, "%s: Requires -p.\n", argv[0]);
+    exit(1);
+  }
+  else if ((pcnt > 100) || (pcnt == 0))
+  {
+    fprintf(stderr, "%s: Invalid percentage value.\n", argv[0]);
+    exit(1);
+  }
   
   assert( init_mytool() == 0);
   
@@ -32,12 +47,16 @@ main(int argc, char *argv[]) {
   finalize_mytool();
   
   if (attempt <= pcnt) {
-    printf("Success!\n");
+    if (vflag)
+      printf("Success\n");
+      
+    exit(0);
   }
   else if (attempt > pcnt) {
-    printf("Failure...\n");
+    if (vflag)
+      printf("Failure\n");
+      
+    exit(1);
   }
-  
-  return 0;
 
 }
